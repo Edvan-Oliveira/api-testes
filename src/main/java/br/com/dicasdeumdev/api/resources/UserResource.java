00e611ba7afd0f207f69589ff084160c151ba30d
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-01-17 19:47:53 -0300

    endpoint delete para user

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +23,15 @@ import br.com.dicasdeumdev.api.services.UserService;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+	private static final String ID = "/{id}";
+
 	@Autowired
 	private ModelMapper mapper;
 	
 	@Autowired
 	private UserService service;
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = ID)
 	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
@@ -42,13 +45,19 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<UserDTO> create(@RequestBody UserDTO dto) {
 		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest().path("/{id}").buildAndExpand(service.create(dto).getId()).toUri();
+				.fromCurrentRequest().path(ID).buildAndExpand(service.create(dto).getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@PutMapping(value = "/{id}")
+	@PutMapping(value = ID)
 	public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO dto) {
 		dto.setId(id);
 		return ResponseEntity.ok(mapper.map(service.update(dto), UserDTO.class));
+	}
+	
+	@DeleteMapping(value = ID)
+	public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
