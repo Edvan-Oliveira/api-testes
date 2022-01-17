3b0816b65b5343e605e1fd8e9e8a3297bf3b5348
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-01-17 15:45:45 -0300

    Ajustes

--- src/main/java/br/com/dicasdeumdev/api/domain/User.java
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	@Column(unique = true)
 	private String email;
 	private String password;
 }
